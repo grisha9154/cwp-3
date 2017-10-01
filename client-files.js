4668009e02f9bb54b45db94b2571f1894eba5654
grisha9154 <grisha9154@gmail.com>
2017-10-02 00:21:03 +0300

    Work Complite

--- client-files.js
+++ client-files.js
@@ -2,42 +2,92 @@ const net = require('net');
 const fs = require('fs');
 const port = 8124;
 
-const adress = [];
+const name = [];
+let idName = 0;
+const fileAdress = [];
+let idFileAdress = 0;
+let sendId =0;
 
-for(let i=0;i<process.argv.length;i++){
-    adress[i] = process.argv[i];
+console.log(process.argv);
+getAllDir(process.argv, function () {
+    const client = new net.Socket();
+    client.setEncoding('utf8');
+
+    client.connect(port, function () {
+        console.log('Connected');
+        client.write('FILES');
+    });
+
+    client.on('data',function (data) {
+        console.log(data);
+        switch (data){
+            case 'ACK':{
+                let sum=0;
+                for(let z = 0;z<9999999;z++){
+                    sum+=z;
+                }
+                console.log(sum);
+                console.log(name);
+                sendAllName(name,client);
+            }break;
+
+            case 'DEC':{
+                return;
+            }
+
+            case 'Send buf':{
+                if(sendId!=fileAdress.length){
+                    fs.readFile(fileAdress[sendId], function (err, data){
+                        sendId++;
+                        const buf = Buffer.from(data);
+                        console.log("Отправил файл "+ name[sendId]);
+                        client.write("Send"+buf);
+                    });}else{
+                    fs.close(0,function () {
+
+                    });
+                    client.write("End");
+                }
+            }break;
+
+        }
+    });
+});
+
+
+
+
+function sendAllName(allName,client) {
+    let str = "";
+    console.log(allName);
+    for(let i = 0; i<allName.length;i++){
+        console.log(i);
+        str +="N"+allName[i];
+    }
+    console.log("Строка имён " +str);
+    client.write(str);
 }
 
-
-
-const client = new net.Socket();
-client.setEncoding('utf8');
-
-client.connect(port, function () {
-    console.log('Connected');
-    client.write('FILES');
-});
-
-client.on('data',function (data) {
-    console.log(data);
-    switch (data){
-        case 'ACK':{
-            client.write(buf);
-        }break;
-    }
-});
-
-function getAllDir(dirPath,copydir) {
-    for (let i = 0; i < dirPath.length; i++) {
-        fs.readdir(dirpath[i], function (err, files) {
+function getAllDir(dirPath,callback) {
+    const i =2;
+        fs.readdir(dirPath[i], function (err, files) {
             if (!err) {
-                for (let i = 0; i < files.length; i++) {
-                    fs.stat(dirPath[i] + "\\" + files[i], function (err, stats) {
-                        if (stats.isFile()) {
-                            fs.readFile(dirPath[i] + '\\' + files[i], function (err, data) {
-                                const buf = Buffer.from(data);
-                                client.write(buf);
-                            })
+                console.log(files);
+                for (let j = 0; j < files.length; j++) {
+                    fs.stat(dirPath[i] + "\\" + files[j], function (err, stats) {
+                        if (!err) {
+                            if (stats.isFile()) {
+                                fs.readFile(dirPath[i] + '\\' + files[j], function (err, data) {
+                                    console.log(data);
+                                    name[idName++] = files[j];
+                                    fileAdress[idFileAdress++] = dirPath[i] + '\\' + files[j];
+                                    if(j===files.length-1){
+                                        callback();
+                                    }
+                                })
+                            } else {
+                                console.log("Ошибка при определении статуса" + dirPath[i] + '\\' + files[j])
+                            }
                         }
                     })
                 }
@@ -45,5 +95,5 @@ function getAllDir(dirPath,copydir) {
                 console.log("Ошибка чтения директории");
             }
         })
-    }
+
 }
--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const fs = require('fs');
 const net = require('net');
 const port = 8124;
 let seed=0;
-const q = [];
-const a = [];
-let QABool = false;
-let FielsBool = false;
+
+
+
+
+
 fs.readFile("E:\\Univer\\5 семестр\\ПСКП\\PSKP\\Лабы\\lab2\\PSKP-cwp-2\\QA.json","utf-8",function (err, copydata) {
+    const q = [];
+    const a = [];
+
+
     let i =0;
     JSON.parse(copydata, function (key,value) {
        if(key!=''){
@@ -18,86 +23,124 @@ fs.readFile("E:\\Univer\\5 семестр\\ПСКП\\PSKP\\Лабы\\lab2\\PSKP-
     });
 
 const server = net.createServer(function(client){
-    fs.open('client_id.log','a',function (err, file_handler) {
-        if(!err){
-            const socetname =Date.now()+seed++;
-            console.log('Client connected');
-            fs.write(file_handler,socetname+": Client connected \n",null, 'utf-8', function(err, written){
-                if(err){
-                    console.log("Ошибка записии");
-                }
-            });
+    if(seed>=process.env.Max_File_User){
+        client.end('DEC','utf-8');
+    }else {
+        let filesName = [];
+        let fileNameId = 0;
+        let fileAddres = process.env.File_Adr;
 
-            client.setEncoding('utf8');
-
-            client.on('data', function(data){
-                console.log('Client say: '+ data);
-
-                fs.write(file_handler,socetname+' say:'+ data+'\n',null, 'utf-8', function(err, written){
-                    if(err){
+        let QABool = false;
+        let FielsBool = false;
+        fs.open('client_id.log', 'a', function (err, file_handler) {
+            if (!err) {
+                const socetname = Date.now() + seed++;
+                console.log('Client connected ' + socetname);
+                fs.write(file_handler, socetname + ": Client connected \n", null, 'utf-8', function (err, written) {
+                    if (err) {
                         console.log("Ошибка записии");
                     }
                 });
-            if(QABool){
-                for(let i=0;i<q.length;i++){
-                    if(q[i]==data){
-                        const str =a[getRandomInt(0,2)];
-                        client.write(str);
-                        fs.write(file_handler,socetname+' answer:'+ str+'\n',null, 'utf-8', function(err, written){
-                            if(err){
-                                console.log("Ошибка записии");
-                            }
+
+                client.setEncoding('utf8');
+
+                client.on('data', function (data) {
+
+                    fs.write(file_handler, socetname + ' say:' + data + '\n', null, 'utf-8', function (err, written) {
+                        if (err) {
+                            console.log("Ошибка записии");
+                        }
+                    });
+
+                    if (data.substring(0, 4) === "Send") {
+                        fs.mkdir(fileAddres + "\\" + socetname, function () {
+                            fs.open(fileAddres + "\\" + socetname + "\\" + filesName[fileNameId], "a", function (err, writer) {
+                                fs.write(writer, data.substring(4), null, 'utf-8', function (err, wr) {
+                                    if (err) {
+                                        console.log("Ошибка записи!!!");
+                                    } else {
+                                        console.log("Файл, который записали " + filesName[fileNameId]);
+                                        fileNameId++;
+                                        client.write("Send buf");
+                                    }
+                                });
+                            });
                         });
                         return;
                     }
-                }
-            }
-                switch (data){
-                    case 'FILES':{
-                        client.write('ACK');
-                        FielsBool = true;
-                        QABool = false;
-                    }break;
-                    case 'QA':
-                    {
-                        client.write('ACK');
-                        QABool = true;
-                        FielsBool = false;
-                        fs.write(file_handler,socetname+' answer:'+ 'ACK'+'\n',null, 'utf-8', function(err, written){
-                            if(err){
-                                console.log("Ошибка записии");
+                    if (QABool) {
+                        for (let i = 0; i < q.length; i++) {
+                            if (q[i] == data) {
+                                const str = a[getRandomInt(0, 2)];
+                                client.write(str);
+                                fs.write(file_handler, socetname + ' answer:' + str + '\n', null, 'utf-8', function (err, written) {
+                                    if (err) {
+                                        console.log("Ошибка записии");
+                                    }
+                                });
+                                return;
                             }
-                        });
-                    }break;
-                    default:
-                    {
-                        client.end('DEC','utf-8');
-                        fs.write(file_handler,socetname+' answer:'+ 'DEC'+'\n',null, 'utf-8', function(err, written){
-                            if(err){
-                                console.log("Ошибка записии");
-                            }
-                        });
-                    }break;
-                }
-
-            });
-
-            client.on('end', function(){ console.log('Client disconnected')
-                fs.write(file_handler,socetname+': Client disconnected\n',null, 'utf-8', function(err, written){
-                    if(err){
-                        console.log("Ошибка записии");
+                        }
                     }
+                    if (FielsBool) {
+                        if (data[0] === 'N') {
+                            filesName = data.substring(1).split("N");
+                            client.write("Send buf");
+
+                            FielsBool = false;
+                            return;
+                        }
+
+                    }
+                    switch (data) {
+                        case 'FILES': {
+                            client.write('ACK');
+                            FielsBool = true;
+                            QABool = false;
+                        }
+                            break;
+                        case 'QA': {
+                            client.write('ACK');
+                            QABool = true;
+                            FielsBool = false;
+                            fs.write(file_handler, socetname + ' answer:' + 'ACK' + '\n', null, 'utf-8', function (err, written) {
+                                if (err) {
+                                    console.log("Ошибка записии");
+                                }
+                            });
+                        }
+                            break;
+                        default: {
+                            client.end('DEC', 'utf-8');
+                            seed--;
+                            fs.write(file_handler, socetname + ' answer:' + 'DEC' + '\n', null, 'utf-8', function (err, written) {
+                                if (err) {
+                                    console.log("Ошибка записии");
+                                }
+                            });
+                        }
+                            break;
+                    }
+
                 });
-            } );
+
+                client.on('end', function () {
+                    console.log('Client disconnected');
+                    fs.write(file_handler, socetname + ': Client disconnected\n', null, 'utf-8', function (err, written) {
+                        if (err) {
+                            console.log("Ошибка записии");
+                        }
+                    });
+                });
 
 
-        }else {
-            console.log("Ошибка открытия");
-        }
-    });
+            } else {
+                console.log("Ошибка открытия");
+            }
+        });
 
 
-});
+    }});
     server.listen(port, function() {
         console.log(`Server listening on localhost:${port}`);});
 });
